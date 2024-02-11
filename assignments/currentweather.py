b39d0bfac658bdd87d9aa3454b9a0af30795631f
Audrey Allen <G00425632@atu.ie>
2024-02-11 17:14:50 +0000

    Assignement 1 - Complete

--- assignments/currentweather.py
+++ assignments/currentweather.py
@@ -10,8 +10,9 @@ import json
 import warnings
 warnings.filterwarnings("ignore")
 
+## The code below is to extract the current temperature in the below location
 
-url = "https://api.open-meteo.com/v1/forecast?latitude=52.71&longitude=-8.5&current=temperature_2m"
+url = "https://api.open-meteo.com/v1/forecast?latitude=52.71&longitude=-8.5003&current=temperature_2m"
 response = requests.get(url,verify = False) ## This was put into the code as due to security settings on my pc it was
 
 data = response.json()
@@ -19,7 +20,23 @@ data = response.json()
 current_units = data["current"]
 
 temperature_2m = current_units["temperature_2m"]
-print(temperature_2m) 	
+
+print (f' Your current temperature is: {temperature_2m}') 
 
 
+
+
+## The code below is to extract the current wind speed in the below location
+
+url = "https://api.open-meteo.com/v1/forecast?latitude=52.71&longitude=-8.5003&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+response = requests.get(url,verify = False) ## This was put into the code as due to security settings on my pc it was
+
+data = response.json()
+
+current_units = data["current"]
+
+wind_speed_10m = current_units["wind_speed_10m"]
+
+print (f' Your current wind speed is: {wind_speed_10m}') 
+ 	
 
