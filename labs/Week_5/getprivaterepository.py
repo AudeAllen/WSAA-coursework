6d2d4d31af67fe158d56ca45612e886b13f12792
Audrey Allen <G00425632@atu.ie>
2024-02-26 13:42:15 +0000

    Week 5 - Labs

--- labs/Week_5/getprivaterepository.py
+++ labs/Week_5/getprivaterepository.py
@@ -5,11 +5,16 @@ import json
 filename = "repos-private.json"
 
 #url = 'https://api.github.com/repos/andrewbeattycourseware/datarepresentation/contents/code'
-url = 'https://github.com/AudeAllen/WSAA-coursework/tree/main/labs/'
+url = 'https://github.com/AudeAllen/WSAA-coursework/tree/main/assignments'
 
 # this token is no longer valid, becuase it has been pushed to GitHub
 # GitHub detects it and invalidates it automatically for security
 apikey='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'
+# the more basic way of setting authorization
+#headers = {'Authorization': 'token ' + apikey}
+#response = requests.get(url, headers= headers)
+
+#apikey = cfg["githubkey"]
 
 
 response = requests.get(url, auth = ('token', apikey),verify = False)
