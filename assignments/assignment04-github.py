a9d759eb7a4aa7b77c8a4bfb8e85bcd31e64b279
Audrey Allen <G00425632@atu.ie>
2024-02-27 20:36:38 +0000

    Delete old config file

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -5,6 +5,7 @@ import requests
 from config import config as cfg
 
 
+
 apikey = cfg["githubkey"]
 
 g = Github(apikey,verify = False)
--- labs/Week_5/config12.py
+++
@@ -1,3 +0,0 @@
-apikeys={
-        "htmltopdfkey":"Z7gXjBALQjCns7EmqO0WKs2VznKP9ErQCvm54Wq9flizbiARkam5LLnSzu1aCxAG"
-        }
